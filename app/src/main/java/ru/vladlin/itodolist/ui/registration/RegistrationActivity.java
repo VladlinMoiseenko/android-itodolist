287d999944eeb0d5dd9f125d14dd7e1df0cfdf9f
Vladlin Moiseenko <mvladlin@gmail.com>
2019-01-06 21:14:19 +0300

    20190106_2114

---
+++ app/src/main/java/ru/vladlin/itodolist/ui/registration/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package ru.vladlin.itodolist.ui.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.vladlin.itodolist.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_registration);
+
+        }
+
+}
